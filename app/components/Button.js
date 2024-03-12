89656f820d7cafdf1c55c03d362cb4fa952b4d93
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-03-12 20:30:32 +0530

    feat:Button Component
    use props to change color variant, size, name, disabled

---
+++ app/components/Button.js
@@ -0,0 +1,28 @@
+"use client"
+import React from "react";
+import {Button} from "@nextui-org/react";
+
+export default function ButtonComponent(props){
+    const {name,variant, color, size, isDisabled} = props;
+
+    return (
+        <Button
+            variant={variant}//solid, faded, bordered, light, flat, ghost, shadow
+            color={color}//default, primary, secondary, success, warning, danger
+            size={size}//sm, md, lg
+            isDisabled={isDisabled === true}//if isDisabled prop is passed then only the button will be disabled
+        >
+            {name}
+        </Button>
+
+
+        //how to use: pass the props to the component
+        // <ButtonComponent
+        //     name="Click me"
+        //     variant="solid"
+        //     color="success"
+        //     size="lg"
+        //     isDisabled  //if u pass this prop then the button will be disabled
+        // />
+    );
+}
