04efa4d1776372033ac0f15dc1a494232530a10a
kaweesha-mr <kaweesha.mr@gmail.com>
2024-03-14 12:06:54 +0530

    feat: props added for grid view to pass data

--- app/components/GridView.js
+++ app/components/GridView.js
@@ -1,24 +1,16 @@
 import * as React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
+import CardTemp from './Card';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 
-export default function GridView() {
+export default function GridView(props) {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
-        {Array.from(Array(6)).map((_, index) => (
-          <Grid xs={2} sm={4} md={4} key={index}>
-            <Item>xs=2</Item>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 2, md: 12 }}>
+        {props.data.map((value) => (
+          <Grid item xs={2} sm={4} md={4} key={value}>
+            <CardTemp title={value.title} secondDes={value.secondaryDescription} description={value.tertiaryDescription} buttons={value.buttons} />
           </Grid>
         ))}
       </Grid>
