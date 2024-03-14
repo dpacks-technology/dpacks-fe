bf1907055191298e708f414bacdf25e92e76defc
kaweesha-mr <kaweesha.mr@gmail.com>
2024-03-14 09:11:06 +0530

    fix:added name attributes for all components as props

--- app/components/TimePicker.js
+++ app/components/TimePicker.js
@@ -5,12 +5,12 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
 
-export default function TimePick(props = { name: "Time Picker" }) {
+export default function TimePick(props = { label: "Select Time" }) {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DemoContainer components={['TimePicker']}>
-                <DemoItem label={props.name}>
-                    <MobileTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
+                <DemoItem label={props.label}>
+                    <MobileTimePicker name={props.name} defaultValue={dayjs('2022-04-17T15:30')} />
                 </DemoItem>
             </DemoContainer>
         </LocalizationProvider>
