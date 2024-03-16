604f30a194b72c7f2b38c9250c40f35f4736cf3c
kaweesha-mr <kaweesha.mr@gmail.com>
2024-03-16 21:44:44 +0530

    feat:Container setup

---
+++ app/pros/Signup/page.js
@@ -0,0 +1,15 @@
+
+import PanelLayout from '@/app/layouts/PanelLayout'
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <div className=' bg-secondaryLight dark:bg-secondaryDark shadow-2xl h-[30rem] w-[50rem] rounded-3xl'>
+                
+        </div>
+    </div>
+  )
+}
+
+export default Signup
