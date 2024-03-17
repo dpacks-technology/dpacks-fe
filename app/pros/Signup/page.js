3b69e97d8fe4265538eedcfbb741077eab44ca4c
kaweesha-mr <kaweesha.mr@gmail.com>
2024-03-17 10:47:58 +0530

    feat:Signup page completed with Next Components
    -Components will be changed after update

--- app/pros/Signup/page.js
+++ app/pros/Signup/page.js
@@ -1,29 +1,89 @@
-
-import React from 'react'
-import Image from 'next/image'
-import InputField from '@/app/components/PersonlizedContent/SIgnUp/EmailInput'
+"use client";
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { Button, Divider, Input } from '@nextui-org/react';
 
 function Signup() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    // Implement your signup logic here
+  };
+
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div className=' bg-secondaryLight dark:bg-secondaryDark shadow-xl h-[30rem] w-[30rem] rounded-3xl'>
-
-        <h1 className='text-4xl m-5 text-center font-bold text-primaryLight dark:text-primaryDark'>Sign Up</h1>
-        <form className='flex flex-col items-center'>
-          <InputField type={"text"} label={"Name"} placeHolder={"Enter Your Name"} />
-          <InputField type={"email"} label={"Email"} placeHolder={"Enter your Email"} />
-          <InputField type={"password"} label={"Password"} placeHolder={"Enter your Passowrd"} />
-          <InputField type={"password"} label={"Re-Password"} placeHolder={"Re-Enter your Passowrd"} />
-            <input type='checkbox' className='m-2' />
-            <p>I accept the <span className='text-primaryLight dark:text-primaryDark'>Privacy Policy</span></p>
-          </div>
-          <button className='bg-primaryLight dark:bg-primaryDark text-secondaryLight dark:text-secondaryDark rounded-full w-1/2 m-5 p-2'>Sign Up</button>
-
-
-        </form>
+      <div className='grid grid-cols-2 bg-secondaryLight dark:bg-secondaryDark shadow-xl h-fit w-[60rem] rounded-3xl'>
+        {/* add image */}
+        <div className='relative h-full w-full'> {/* Make sure this div is the container for the image */}
+          <Image
+            className='rounded-l-3xl'
+            src='/images/PersonolizedContent/Background.jpg'
+            layout='fill'
+            objectFit='cover' // This makes the image cover the available space
+            quality={100} // Optional: Adjust quality
+          />
+        </div>
+        
+        <div>
+          <h1 className='text-4xl m-5 text-center font-bold text-primaryLight dark:text-primaryDark'>Sign Up</h1>
+          <form className='flex flex-col items-center' onSubmit={handleSignup}>
+            <Input 
+              type="text" 
+              variant="faded" 
+              label="Name" 
+              placeholder="Enter Your Name" 
+              size="sm" 
+              radius="full" 
+              className="w-80 text-black m-2"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input 
+              type="email" 
+              variant="faded" 
+              label="Email" 
+              placeholder="Enter Your Email" 
+              size="sm" 
+              radius="full" 
+              className="w-80 text-black m-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+              type="password" 
+              variant="faded" 
+              label="Password" 
+              placeholder="Enter Your Password" 
+              size="sm" 
+              radius="full" 
+              className="w-80 text-black m-2"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Input 
+              type="password" 
+              variant="faded" 
+              label="Re-Password" 
+              placeholder="Re-Enter your Password" 
+              size="sm" 
+              radius="full" 
+              className="w-80 text-black m-2"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <Button color="primary" className='w-52 mt-5' variant="shadow" type="submit">
+              Sign Up
+            </Button>
+            <Divider className='w-fit mt-5' />
+          </form>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
