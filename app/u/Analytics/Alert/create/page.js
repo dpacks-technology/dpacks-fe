abb1f82bc10120c9c5f7dda43ba93de7fcc87b76
Yashodha Fernando <118588673+Yashoda-Fr@users.noreply.github.com>
2024-03-18 01:11:46 +0530

    feat:update create pafe

--- app/u/Analytics/Alert/create/page.js
+++ app/u/Analytics/Alert/create/page.js
@@ -2,7 +2,51 @@
 import {Flex, Input} from 'antd';
 import React, { useState } from 'react';
 import { Radio } from 'antd';
+import { Button } from 'antd';
+import {  Dropdown, Space } from 'antd';
 
+function handleMenuClick(e) {
+    console.log('click', e);
+}
+
+const items = [
+    {
+        key: '1',
+        label: (
+
+            "Hourly"
+
+        ),
+    },
+    {
+        key: '2',
+        label: (
+
+            "Daily"
+
+        ),
+    },
+    {
+        key: '3',
+        label: (
+
+            "Weekly"
+
+        ),
+    },
+    {
+        key: '4',
+        label: (
+
+            "CustomDate"
+
+        ),
+    },
+];
+const menuProps = {
+    items,
+    onClick: handleMenuClick,
+};
 
 
 
@@ -13,23 +57,49 @@ export default function Create(){
             <h1 className="text-center text-3xl font-bold m-10">Create Alert</h1>
             <form className="p-7 ">
 
-                <div className="flex justify-around ">
+                <div className=" ">
                     <label className="text-lg font-bold">Name</label>
-                    <Input className={"w-96 h-12"} placeholder="Basic usage"/>
-                    <label className="text-lg font-bold">Name</label>
-                    <Input className={"w-96"} placeholder="Basic usage"/>
+                    <Input className={"w-96 h-12"} placeholder="Name"/>
+
                 </div>
-                <div className="flex mt-3 justify-around ">
+                <div className="mt-6">
+                    <label className="text-lg font-bold">Trigger Count</label>
+                    <Input className={"w-96 h-12"} placeholder="Trigger Count"/>
+                </div>
+
+                <div className="flex justify-start  mt-10 ">
 
                     <Flex vertical gap="middle">
-                        <Radio.Group defaultValue="a" buttonStyle="solid">
-                            <Radio.Button value="a">Hangzhou</Radio.Button>
-                            <Radio.Button value="b">Shanghai</Radio.Button>
-                            <Radio.Button value="c">Beijing</Radio.Button>
-                            <Radio.Button value="d">Chengdu</Radio.Button>
+                        <Radio.Group className="h-16" defaultValue="a" buttonStyle="solid">
+                            <label className="text-lg font-bold text-white">Duration</label>
+                            <Radio.Button value="a">Immediate</Radio.Button>
+                            <Radio.Button value="b">Before Reach</Radio.Button>
+                            <Radio.Button value="c">Both</Radio.Button>
+                            {/*<Radio.Button value="d">Chengdu</Radio.Button>*/}
                         </Radio.Group>
                     </Flex>
 
+
+
+
+
+
+
+                </div>
+
+                <div>
+                    <label className="text-lg font-bold text-white">Reminder Option</label>
+                    <Dropdown
+                        menu={{
+                            items,
+                        }}
+                        placement="bottom"
+                    >
+                        <Button className="text-lg font-black " size="large">Options</Button>
+                    </Dropdown>
+                </div>
+                <div className="mt-20 flex justify-center">
+                    <Button>Create Alert</Button>
                 </div>
 
 
