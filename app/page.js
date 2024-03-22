bb836a86b04b507794452205928f832ea41fad43
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-03-22 16:36:32 +0530

    refactor: homepage base

--- app/page.js
+++ app/page.js
@@ -1,17 +1,13 @@
 'use client';
 
 import React from "react";
-import CardBox from "@/app/components/Card";
 
 export default function Home() {
 
-    function testFunction() {
-        console.log("Test");
-    }
-
     return (
-        <main className="flex flex-col items-center justify-between p-24 bg-secondaryLight dark:bg-secondaryDark">
-
+        <main className="bg-light dark:bg-dark">
+            Homepage
         </main>
     );
+
 }
