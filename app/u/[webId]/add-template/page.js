fe08e74673729c46d8dd7cc27d6a5e9beeb1b01f
IshiniAponso <ishiniaponso@gmail.com>
2024-03-28 14:40:15 +0530

    feat:add-template-form

--- app/u/[webId]/add-template/page.js
+++ app/u/[webId]/add-template/page.js
@@ -1,101 +1,158 @@
-import ButtonComponent from "@/app/components/Button";
-import Input from "@/app/components/Input";
+'use client'
 import React from 'react';
-import Checkbox from 'antd/lib/Checkbox';
+import { Form, Input, Select, InputNumber, Upload, Button, Space} from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
 
-
+//Function to add a template to the marketplace
 export default function MarketplaceAddTemplate(){
-    // const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-    // const CheckboxGroup = Checkbox.Group;
-    //
-    // const options = [
-    //     {
-    //         label: 'Apple',
-    //         value: 'Apple',
-    //     },
-    //     {
-    //         label: 'Pear',
-    //         value: 'Pear',
-    //     },
-    //     {
-    //         label: 'Orange',
-    //         value: 'Orange',
-    //     },
-    // ];
+    // Define layout for form
+    const layout = {
+        labelCol: {
+            span: 8,
+        },
+        wrapperCol: {
+            span: 16,
+        },
+    };
+
+    // Option from Select
+    const { Option } = Select;
+
+    // Define suffix selector for InputNumber
+    const suffixSelector = (
+        <Form.Item name="suffix" noStyle>
+            <Select
+                style={{
+                    width: 70,
+                }}
+            >
+                <Option value="USD">$</Option>
+                <Option value="CNY">¥</Option>
+            </Select>
+        </Form.Item>
+    );
+
+    // Normalize file for upload
+    const normFile = (e) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e?.fileList;
+    };
+
+    // Return JSX for the component
     return (
         <>
             <h1 className="mt-4 ml-6">Add Template to the Marketplace</h1><br/>
-            <form className="p-6 w-960">
+            <Form className="p-6 w-960">
                 <div className="flex justify-between">
                     <div className="bg-gray-200 bg-opacity-50 rounded-lg w-1/2 pr-10 p-6">
-                        <h2 className="text-black">Template Details:</h2> <br/>
-                        <Input label={"Template Name"} color={"primary"}/> <br/><br/>
-                        <Input label={"Description"} color={"primary"}/> <br/><br/>
-                        <Input label={"Category"} color={"primary"}/> <br/><br/>
-                        {/*<div className="text-black text-sm">*/}
-                        {/*    <p>Features:</p>*/}
-                        {/*    <Checkbox {...label} /> Responsive*/}
-                        {/*    <Checkbox {...label} /> Multi-Page*/}
-                        {/*    <Checkbox {...label} /> Single-Page<br/>*/}
-                        {/*    <Checkbox {...label} /> Interactive*/}
-                        {/*</div>*/}
+                        <h2 className="text-lg text-black">Template Details:</h2>
+                        <Form.Item
+                            label="Template Name"
+                            name="templatename"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Name of the Template is required!',
+                                },
+                            ]}
+                        >
+                            <Input/>
+                        </Form.Item>
 
-                        {/*<CheckboxGroup options={options} defaultValue={['Pear']}  />*/}
+                        <Form.Item name={['user', 'tempDescription']} label="Template Description" className="p-3">
+                            <Input.TextArea/>
+                        </Form.Item>
+
+                        <Form.Item label="Template Category" className="p-3">
+                            <Select>
+                                <Option value="Blog & Magazine">Blog & Magazine</Option>
+                                <Option value="Business & Corporate">Business & Corporate</Option>
+                                <Option value="E-commerce">E-commerce</Option>
+                                <Option value="Education & Learning">Education & Learning</Option>
+                                <Option value="Fashion & Beauty">Fashion & Beauty</Option>
+                                <Option value="Food & Restaurant">Food & Restaurant</Option>
+                                <Option value="Health & Fitness">Health & Fitness</Option>
+                                <Option value="Real Estate">Real Estate</Option>
+                                <Option value="Portfolio & Resume">Portfolio & Resume</Option>
+                                <Option value="Technology & Apps">Technology & Apps</Option>
+                                <Option value="Travel & Tourism">Travel & Tourism</Option>
+                                <Option value="Other">Other</Option>
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            name="tempPrice"
+                            label="Price Suggestion"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input donation amount!',
+                                },
+                                {
+                                    type: 'number',
+                                    min: 0, // Minimum value allowed
+                                    max: 100, // Maximum value allowed
+                                    message: 'Template price must be between $0 and $100!',
+                                },
+                            ]}
+                        >
+                            <InputNumber
+                                addonAfter={suffixSelector}
+                                style={{
+                                    width: '100%',
+                                }}
+                            />
+                        </Form.Item>
+
 
                     </div>
                     <div className="bg-gray-200 bg-opacity-50 rounded-lg w-1/2 pl-10 p-6 ml-4">
-                        {/*<h2>Template Files:</h2><br/>*/}
-                        {/*<p className="text-black text-sm">Upload Template File (zip/rar):</p>*/}
-                        {/*<input type="file" name="file" accept=".zip,.rar" className="mb-4 text-black text-sm"/> <br/>*/}
-                        {/*<p className="text-black text-sm">Upload Template Thumbnail (jpg/png):</p>*/}
-                        {/*<input type="file" name="file" accept=".jpg,.png" className="mb-4 text-black text-sm"/> <br/>*/}
-                        {/*<br/>*/}
-                        <h2 className="text-black">Template Files:</h2><br/>
-                        <div className="mb-4">
-                            <label htmlFor="templateFile" className="block text-sm text-gray-700 mb-1">Upload Template
-                                File (ZIP/RAR):</label>
-                            <input id="templateFile" type="file" name="file" accept=".zip,.rar" className="hidden"/>
-                            <label htmlFor="templateFile"
-                                   className="bg-blue-500 text-white text-sm py-2 px-4 rounded-md cursor-pointer hover:bg-blue-600">Choose
-                                File</label>
-                        </div>
+                        <h2 className="text-lg text-black">Files:</h2>
+                        <Form.Item label="Upload Template">
+                            <Form.Item name="draggerTemp" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                                <Upload.Dragger name="files" action="/upload.do">
+                                    <p className="ant-upload-drag-icon">
+                                        <InboxOutlined/>
+                                    </p>
+                                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                    <p className="ant-upload-hint">Upload as a .zip folder.</p>
+                                </Upload.Dragger>
+                            </Form.Item>
+                        </Form.Item>
 
-                        <div className="mb-4">
-                            <label htmlFor="thumbnailFile" className="block text-sm text-gray-700 mb-1">Upload Template
-                                Thumbnail (JPG/PNG):</label>
-                            <input id="thumbnailFile" type="file" name="file" accept=".jpg,.png" className="hidden"/>
-                            <label htmlFor="thumbnailFile"
-                                   className="bg-blue-500 text-white text-sm py-2 px-4 rounded-md cursor-pointer hover:bg-blue-600">Choose
-                                File</label>
-                        </div>
-                        <br/><br/>
-                        <h2 className="text-black">Developer Details:</h2><br/>
+                        <h2 className="text-lg text-black">Developer Details:</h2>
+                        <Form.Item
+                            name={['user', 'Dname']}
+                            label="Developer's Name"
+                            rules={[
+                                {
+                                    required: true,
+                                },
+                            ]}
+                        >
+                            <Input/>
+                        </Form.Item>
 
-                        <Input label={"Developer's Name"} color={"primary"}/> <br/><br/>
-                        <Input label={"Developer's Message"} color={"primary"}/> <br/>
+                        <Form.Item name={['user', 'tempDescription']} label="Template Message" className="p-3">
+                            <Input.TextArea/>
+                        </Form.Item>
 
                     </div>
                 </div>
                 <div className="flex justify-end mt-8">
-                    <ButtonComponent
-                        name="Add Template"
-                        variant="solid"
-                        color="primary"
-                        size="sm"
-                        //isDisabled
-                    />
-                    <div className="w-4"></div>
-                    <ButtonComponent
-                        name="Clear"
-                        variant="solid"
-                        color="secondary"
-                        size="sm"
-                        //isDisabled
-                    />
-
+                    <Space direction="horizontal" style={{ maxWidth: 600, margin: '0 auto' }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                        <Button htmlType="reset">reset</Button>
+                    </Space>
                 </div>
-            </form>
+            </Form>
+
 
         </>
-    )
+)
 }
