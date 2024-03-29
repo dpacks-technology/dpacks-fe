b358dc685ec65bacf7be5a8ab33dbc9f33fe3673
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-03-29 15:49:04 +0530

    backend connected with axios

--- services/WebpagesService.js
+++ services/WebpagesService.js
@@ -9,7 +9,7 @@ const webpagesService = axios.create({
 // get all webpages // TODO: Change this function accordingly
 export const getWebPages = async (count, page, key, val) => {
     try {
-        const response = await webpagesService.get(`/api/web/webpages/${count}/${page}?key=${key}&val=${val}`);
+        const response = await webpagesService.get(`http://localhost:4000/api/pros/Access/AccessLists/${count}/${page}?key=${key}&val=${val}`);
         return response.data;
     } catch (error) {
         throw error;
@@ -19,41 +19,7 @@ export const getWebPages = async (count, page, key, val) => {
 // get all webpages count // TODO: Change this function accordingly
 export const getWebPagesCount = async (key, val) => {
     try {
-        const response = await webpagesService.get(`/api/web/webpages/count?key=${key}&val=${val}`);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// get all webpages by status // TODO: Change this function accordingly
-export const getPagesByStatus = async (count, page, status, key, val) => {
-
-    // convert status array to string with commas
-    let statusString = status.join(',');
-
-    console.log(statusString);
-
-    try {
-        const response = await webpagesService.get(`/api/web/webpages/status/${count}/${page}?status=${statusString}&key=${key}&val=${val}`);
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// get all webpages by status count // TODO: Change this function accordingly
-export const getPagesByStatusCount = async (status, key, val) => {
-
-    // convert status array to string with commas
-    let statusString = status.join(',');
-
-    console.log(statusString);
-
-    try {
-        const response = await webpagesService.get(`/api/web/webpages/status/count?status=${statusString}&key=${key}&val=${val}`);
-        console.log(response.data);
+        const response = await webpagesService.get(`http://localhost:4000/api/pros/Access/AccessList/count?key=${key}&val=${val}`);
         return response.data;
     } catch (error) {
         throw error;
@@ -66,7 +32,7 @@ export const getPagesByDatetime = async (count, page, start, end, key, val) => {
     console.log(start);
 
     try {
-        const response = await webpagesService.get(`/api/web/webpages/datetime/${count}/${page}?start=${start}&end=${end}&key=${key}&val=${val}`);
+        const response = await webpagesService.get(`http://localhost:4000/api/pros/web/webpages/datetime/${count}/${page}?start=${start}&end=${end}&key=${key}&val=${val}`);
         console.log(response.data);
         return response.data;
     } catch (error) {
@@ -77,7 +43,7 @@ export const getPagesByDatetime = async (count, page, start, end, key, val) => {
 // get all webpages by datetime count // TODO: Change this function accordingly
 export const getPagesByDatetimeCount = async (start, end, key, val) => {
     try {
-        const response = await webpagesService.get(`/api/web/webpages/datetime/count?start=${start}&end=${end}&key=${key}&val=${val}`);
+        const response = await webpagesService.get(`http://localhost:4000/api/pros/Access/AccessList/datetime/count?start=${start}&end=${end}&key=${key}&val=${val}`);
         console.log(response.data);
         return response.data;
     } catch (error) {
@@ -85,76 +51,4 @@ export const getPagesByDatetimeCount = async (start, end, key, val) => {
     }
 }
 
-// get all webpages by status and datetime // TODO: Change this function accordingly
-export const editPages = async (id, data) => {
-    try {
-        const response = await webpagesService.put(`/api/web/webpages/${id}`, data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
 
-// get all webpages by status and datetime count // TODO: Change this function accordingly
-export const getPageById = async (id) => {
-    try {
-        const response = await webpagesService.get(`/api/web/webpage/${id}`);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// get all webpages by status and datetime count // TODO: Change this function accordingly
-export const deletePage = async (id) => {
-    try {
-        const response = await webpagesService.delete(`/api/web/webpages/${id}`);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// get all webpages by status and datetime count // TODO: Change this function accordingly
-export const deleteWebpagesBulk = async (ids) => {
-    let idsString = ids.join(',');
-
-    try {
-        const response = await webpagesService.delete(`/api/web/webpages/bulk/${idsString}`);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// get all webpages by status and datetime count // TODO: Change this function accordingly
-export const updateWebpagesStatusBulk = async (ids, status) => {
-    let idsString = ids.join(',');
-
-    try {
-        const response = await webpagesService.put(`/api/web/webpages/status/bulk/${idsString}`, {status: status});
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// get all webpages by status and datetime count // TODO: Change this function accordingly
-export const updateWebpagesStatus = async (id, status) => {
-    try {
-        const response = await webpagesService.put(`/api/web/webpages/status/${id}`, {status: status});
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// get all webpages by status and datetime count // TODO: Change this function accordingly
-export const AddWebpage = async (data) => {
-    try {
-        const response = await webpagesService.post(`/api/web/webpage`, data);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
