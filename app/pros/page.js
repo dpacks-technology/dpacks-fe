0cfd14787565186bd71ddc02bfac542bfe1e9fa9
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-03-30 10:12:50 +0530

    init

---
+++ app/pros/page.js
@@ -0,0 +1,13 @@
+'use client';
+
+import React from "react";
+
+export default function Home() {
+
+    return (
+        <main className="bg-light dark:bg-dark">
+            Homepage
+        </main>
+    );
+
+}
