45580c248e34644fc7343a9ad4afbbfb44e8bea0
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-04-03 13:12:04 +0530

    feat:Text area component updated to pass any prop

--- app/components/TextArea.js
+++ app/components/TextArea.js
@@ -1,32 +1,12 @@
 "use client"
-import TextField from '@mui/material/TextField';
+import { Input } from 'antd';
+const { TextArea } = Input;
 
 
-export default function TextArea(props){
-    const { id, name, label, placeholder,width, height} = props;
+export default function Textarea({ ...props }) {
     return (
 
-        <TextField
-            id={id}
-            name={name}
-            label={label}
-            placeholder={placeholder}
-            multiline
-            className={`${width} ${height}`}
-        />
-
-
-        //how to use: pass the props to the component, width should be as tailwindcss classes
-        //     <TextArea
-        //         id="text-area"
-        //         name="text-area"
-        //         label="Text Area"
-        //         placeholder="Enter your text here"
-        //         width="w-96"
-        //         height="h-96"
-        //     />
-
-
+        <TextArea rows={4} placeholder="maxLength is 6" {...props} maxLength={6} />
 
     );
 }
