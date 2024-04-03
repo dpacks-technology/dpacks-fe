83eec6b8cd82f6edcc0a7b78ff5f6c2675b6cbc2
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-04-03 17:15:49 +0530

    fix: button visability issues fixed

--- app/pros/dashbord/Profile/page.js
+++ app/pros/dashbord/Profile/page.js
@@ -15,13 +15,7 @@ export default function Profile() {
     const [lastName, setLastName] = useState("");
     const [phone, setPhone] = useState("");
     const [favouriteCategories, setFavouriteCategories] = useState([]);
-
-    const data = [
-        { id: 1, status: 'processing', color: 'processing' },
-        { id: 2, status: 'completed', color: 'success' },
-        { id: 3, status: 'pending', color: 'warning' },
-        // Add more data as needed
-    ];
+    const [isButtonVisible, setIsButtonVisible] = useState(true);
 
     // useEffect hook to fetch data when the component mounts
     useEffect(() => {
@@ -52,8 +46,12 @@ export default function Profile() {
         <div>
             <h1 className="text-center text-4xl font-bold">Profile</h1>
             <div className="flex justify-end">
-                <button id="editbtn" className="text-xs text-light dark:text-dark"
-                    onClick={() => setIsDisabled(!isDisabled)}
+                <button id="editbtn" className={`text-xs text-light dark:text-dark ${isButtonVisible ? '' : 'hidden'}`}
+                    
+                    onClick={() => {
+                        setIsDisabled(!isDisabled)
+                        setIsButtonVisible(!isButtonVisible);
+                    }}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
@@ -84,7 +82,12 @@ export default function Profile() {
 
             <div className="flex justify-center gap-5 mt-5">
                 <button className={`bg-success-50 text-light dark:text-dark px-5 py-2 rounded-lg ${isDisabled ? 'hidden' : ''}`}>Save</button>
-                <button onClick={() => setIsDisabled(!isDisabled)} className={`bg-success-50 text-light dark:text-dark px-5 py-2 rounded-lg ${isDisabled ? 'hidden' : ''}`}>Cancel</button>
+                <button onClick={() =>{ 
+                    setIsDisabled(!isDisabled)
+                    setIsButtonVisible(!isButtonVisible);
+                }
+                    
+                    } className={`bg-success-50 text-light dark:text-dark px-5 py-2 rounded-lg ${isDisabled ? 'hidden' : ''}`}>Cancel</button>
             </div>
         </div>
     );
