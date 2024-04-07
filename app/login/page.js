3e2d4d267b9fb9beb9cc9aa08066b151c1130a62
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-04-07 13:33:20 +0530

    chore: login page

--- app/login/page.js
+++ app/login/page.js
@@ -12,23 +12,23 @@ export default function LoginPage() {
 
     return (
         <>
-            <div className={"w-full con-mid"} style={{minHeight: "100vh"}}>
-                <div className={"dark:bg-secondaryDark w-full lg:w-1/2 p-12 pb-6 rounded-2xl"}>
-                    <div className={"grid grid-cols-3 gap-12"}>
-                        <div className={"col-span-1"}>
+            <div className={"w-full con-mid p-6 lg:p-0 overflow-hidden"} style={{minHeight: "100vh"}}>
+                <div className={"dark:bg-secondaryDark w-full sm:w-3/4 lg:w-1/2 p-6 lg:p-12 rounded-2xl"}>
+                    <div className={"md:grid md:grid-cols-3 md:gap-12"}>
+                        <div className={"col-span-1 p-2"}>
                             <div className={"w-full text-left"}>
-                                <div className={"grid grid-cols-1 h-full gap-32"}>
-                                    <div className={""}>
+                                <div className={"md:grid grid-cols-1 md:gap-24"}>
+                                    <div>
                                         <img src={"/images/logos/dpacks-logo-white.png"}
-                                             className={"w-4/5"} alt={"DPacks Logo"}/>
-                                        <h1 className={"dark:text-dark mt-3"}>Sign in</h1>
+                                             className={"w-1/2 sm:w-1/2 md:w-3/4 lg:w-full xl:w-4/5"} alt={"DPacks Logo"}/>
+                                        <h1 className={"dark:text-dark mt-3 text-md sm:text-lg mb-3 sm:mb-0"}>Sign in</h1>
                                     </div>
-                                    <div className={""}>
+                                    <div className={"hidden md:block"}>
                                         <p style={{fontSize: "10px"}} className={"mb-2"}>
                                             © {new Date().getFullYear()} DPacks. All rights reserved.
                                         </p>
                                         <p style={{fontSize: "9px"}} className={"dark:text-darkSecondary"}>
-                                            Technology for static web data management and global distribution.
+                                             Static data management and global distribution technology.
                                         </p>
                                     </div>
                                 </div>
@@ -51,10 +51,13 @@ export default function LoginPage() {
                                         type={"password"}
                                     />
                                 </FormItem>
-                                <FormItem>
-                                    <Button variant={"flat"} color={"primary"} className={"w-full mt-2"}>Sign in</Button>
-                                </FormItem>
+                                <Button variant={"flat"} color={"primary"} className={"w-full mt-2"}>Sign in</Button>
                             </Form>
+                        </div>
+                        <div className={"block md:hidden text-center"}>
+                            <p style={{fontSize: "10px"}} className={"mb-2 mt-3 dark:text-darkSecondary"}>
+                                © {new Date().getFullYear()} DPacks. All rights reserved.
+                            </p>
                         </div>
                     </div>
                 </div>
