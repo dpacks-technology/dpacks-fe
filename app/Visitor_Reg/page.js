138d0572d9d596153cbe5dbcb80cf2ccc7ff9e18
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-04-11 07:42:32 +0530

    feat:visitor signup page updated to slider

--- app/Visitor_Reg/page.js
+++ app/Visitor_Reg/page.js
@@ -1,166 +1,300 @@
 "use client"
-import { useState } from 'react';
-import { Button, Divider, Input, Textarea, Select, Chip, SelectItem } from '@nextui-org/react';
-import { users, animals } from '@/app/components/PersonlizedContent/SIgnUp/data';
 
-function Signup() {
+import { Form, Select } from "antd";
+import FormItem from "antd/es/form/FormItem";
+import Input from "@/app/components/Input";
+import { Button } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MultiSelect from "../components/SelectWithTag";
+import { users } from "../components/PersonlizedContent/SIgnUp/data";
+
+export default function GetNamePage() {
+  const [page, setPage] = useState(0);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [selectedUsers, setSelectedUsers] = useState([]); // Assuming this is the correct initial state
-  const [selectedGender, setSelectedGender] = useState(null);
-  const [colorTheme, setColorTheme] = useState(null);
+  const [email, setEmail] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [gender, setGender] = useState("male");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isInvalidConfirmPassword, setIsInvalidConfirmPassword] = useState(false);
+  const [confirmPasswordTouched, setConfirmPasswordTouched] = useState(false);
+  const [IsPasswordEmpty, setIsPasswordEmpty] = useState(false);
+  const [colorTheme, setColorTheme] = useState("dark");
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
-
-
-  const handleSignup = (e) => {
-    e.preventDefault();
-    
+  const handleSelectionChange = (selectedItemNames) => {
+    // Assuming 'items' uses 'name' as a unique key; adjust logic if 'id' should be used instead
+    const updatedSelection = users.filter(user => selectedItemNames.includes(user.name));
+    setSelectedUsers(updatedSelection);
+    console.log("Selected Users: ", updatedSelection);
   };
 
+
+  useEffect(() => {
+    if (password !== confirmPassword && confirmPasswordTouched) {
+      setIsInvalidConfirmPassword(true);
+    } else {
+      setIsInvalidConfirmPassword(false);
+    }
+  }, [password, confirmPassword]);
+
+  useEffect(() => {
+    if (password === "") {
+      setIsPasswordEmpty(true);
+    } else {
+      setIsPasswordEmpty(false);
+    }
+  }, [password]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      phone: phone,
+      date_of_birth: dateOfBirth,
+      gender: gender,
+      password: password,
+    };
+
+    //send data to backend using
+    try {
+      const response = await axios.post(`http://localhost:4010/api/auth/signup`, data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+
+
+
+  }
+
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='bg-secondaryLight dark:bg-secondaryDark shadow-xl h-fit w-[60rem] rounded-3xl'>
-        <div>
-          <h1 className='text-4xl m-5 text-center font-bold text-primaryLight dark:text-primaryDark'>Sign Up</h1>
-          <form className='grid grid-cols-2 items-center' onSubmit={handleSignup}>
-            <div className='flex flex-col mt-5 items-center gap-5'>
-              <Input
-                type="text"
-                variant="faded"
-                label="First Name"
-                placeholder="Enter Your First Name"
-                size="sm"
-                className="w-80 text-black m-2"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-              <Input
-                type="email"
-                variant="faded"
-                label="Email"
-                placeholder="Enter Email"
-                size="sm"
-                className="w-80 text-black m-2"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {/* Assuming another input or a date picker should be here */}
-              <Select
-                items={users}
-                variant="bordered"
-                isMultiline={true}
-                selectionMode="multiple"
-                placeholder="Select Categories"
-                labelPlacement="outside"
-                classNames={{
-                  base: "max-w-xs",
-                  trigger: "min-h-unit-12 py-2",
-                }}
-                selectedItems={selectedUsers}
-                onSelectionChange={(newSelection) => setSelectedUsers(newSelection)}
-                renderValue={(items) => {
-                  return (
-                    <div className="flex flex-wrap max-h-16 overflow-scroll gap-2">
-                      {items.map((item) => (
-                        <Chip key={item.key}>{item.data.name}</Chip>
-                      ))}
-                    </div>
-                  );
-                }}
-              >
-                {(user) => (
-                  <SelectItem key={user.id} textValue={user.name}>
-                    <div className="flex gap-2 items-center">
-                      <div className="flex flex-col ">
-                        <span className="text-small">{user.name}</span>
+    <>
+      <div className={"flex items-center justify-center min-h-screen p-6"} style={{ minHeight: "100vh" }}>
+        <div className={"dark:bg-secondaryDark w-full sm:w-3/4 lg:w-1/2 p-6 lg:p-12 rounded-2xl"}>
+          <div className={"md:grid md:grid-cols-3 md:gap-12"}>
+            <div className={"col-span-1 p-2"}>
+              <div className={"w-full text-left"}>
+                <div className={"md:grid grid-cols-1 md:gap-24"}>
+                  <div>
+                    <img src={"/images/logos/dpacks-logo-white.png"}
+                      className={"w-1/2 sm:w-1/2 md:w-3/4 lg:w-full xl:w-4/5"} alt={"DPacks Logo"} />
+                    <h1 className={"dark:text-dark mt-3 text-md sm:text-lg mb-3 sm:mb-0"}>Sign in</h1>
+                  </div>
+                  <div className={"hidden md:block"}>
+                    <p style={{ fontSize: "10px" }} className={"mb-2"}>
+                      ©️ {new Date().getFullYear()} DPacks. All rights reserved.
+                    </p>
+                    <p style={{ fontSize: "9px" }} className={"dark:text-darkSecondary"}>
+                      Static data management and global distribution technology.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={"col-span-2 p-2"}>
+              <Form>
+                {page === 0 ? (
+                  <>
+                    <FormItem className={"md:mt-5"}>
+                      <Input
+                        type="text"
+                        variant="faded"
+                        label="First Name"
+                        placeholder="Enter Your First Name"
+                        size="sm"
+                        className="w-80 text-black m-2"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                      />
+                    </FormItem>
+                    <FormItem>
+                      <Input
+                        type="text"
+                        variant="faded"
+                        label="Last Name"
+                        placeholder="Enter Your Last Name"
+                        size="sm"
+                        className="w-80 text-black m-2"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                      />
+                    </FormItem>
+                    <FormItem>
+                      <div className="flex flex-row justify-end">
+                        <Button variant={"flat"} color={"primary"} className={"md:w-2/6 w-full"} onClick={() => setPage(1)}>
+                          Next
+                        </Button>
                       </div>
+
+
+                    </FormItem>
+                  </>
+                ) : page === 1 ? (
+                  <>
+                    <FormItem className="md:mt-2">
+                      <Input
+                        type="email"
+                        variant="faded"
+                        label="Email"
+                        placeholder="Enter Email"
+                        size="sm"
+                        className="w-80 text-black m-2"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </FormItem>
+                    <FormItem>
+                      <Input
+                        type="phone"
+                        variant="faded"
+                        label="Phone"
+                        placeholder="Enter Phone Number"
+                        size="sm"
+                        className="w-80 text-black m-2"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                    </FormItem>
+                    <FormItem >
+                      <div className="flex flex-col gap-5 md:flex-row md:justify-between">
+                        <Button variant={"flat"} color={"primary"} onClick={() => setPage(0)}>
+                          Back
+                        </Button>
+                        <Button variant={"flat"} color={"primary"} onClick={() => setPage(2)}>
+                          Next
+                        </Button>
+                      </div>
+                    </FormItem>
+                  </>
+                ) : page === 2 ? (
+                  <>
+                    <FormItem className="md:mt-2">
+                      <Input
+                        label={"Date of Birth"}
+                        className={"w-full"}
+                        placeholder={"Date of Birth"}
+                        type={"date"}
+                        value={dateOfBirth}
+                        onChange={(e) => setDateOfBirth(e.target.value)}
+                      />
+                      <Select
+                        value={gender}
+                        onChange={(value) => setGender(value)}
+                        options={[
+                          { value: 'male', label: 'Male' },
+                          { value: 'female', label: 'Female' },
+                        ]}
+                        className="mt-3"
+                      />
+                      <Select
+                        label={"Color Theme"}
+                        value={colorTheme}
+                        onChange={(value) => setColorTheme(value)}
+                        options={[
+                          { value: 'dark', label: 'Dark' },
+                          { value: 'light', label: 'Light' },
+                        ]}
+                        className="mt-3"
+                      />
+                    </FormItem>
+                    <FormItem>
+                      <div className="flex flex-col gap-5 md:flex-row md:justify-between">
+                        <Button variant={"flat"} color={"primary"} onClick={() => setPage(1)}>
+                          Back
+                        </Button>
+                        <Button variant={"flat"} color={"primary"} onClick={() => setPage(3)}>
+                          Next
+                        </Button>
+                      </div>
+                    </FormItem>
+                  </>
+
+                ) : page === 3 ? (
+                  <>
+                    <FormItem className="md:mt-2">
+                      <MultiSelect
+                        items={users}
+                        selectedItems={selectedUsers.map(user => user.name)}
+                        onChange={handleSelectionChange}
+                        placeholder="Select Categories"
+                        labelOutside={true}
+                      />
+
+                    </FormItem>
+                    <FormItem>
+                      <div className="flex flex-col gap-5 md:flex-row md:justify-between">
+                        <Button variant={"flat"} color={"primary"} onClick={() => setPage(2)}>
+                          Back
+                        </Button>
+                        <Button variant={"flat"} color={"primary"} onClick={() => setPage(4)}>
+                          Next
+                        </Button>
+                      </div>
+                    </FormItem>
+                  </>
+
+                ) : page === 4 ? (
+                  <>
+                    <FormItem>
+                      <Input
+                        label={"Password"}
+                        className={"w-full"}
+                        placeholder={"Password"}
+                        type={"password"}
+                        value={password}
+                        onChange={(e) => {
+                          setPassword(e.target.value)
+                        }}
+                        status={password === "" ? "error" : ""}
+                        error={password === "" ? "Password Cannot be empty" : ""}
+                      />
+                    </FormItem>
+                    <FormItem>
+                      <Input
+                        label={"Confirm Password"}
+                        className={"w-full"}
+                        placeholder={"Confirm Password"}
+                        type={"password"}
+                        value={confirmPassword}
+                        onChange={(e) => {
+                          setConfirmPassword(e.target.value);
+                          setConfirmPasswordTouched(true);
+
+                        }}
+                        status={isInvalidConfirmPassword ? "error" : ""}
+                        error={isInvalidConfirmPassword ? "Passwords do not match" : ""}
+                      />
+
+                    </FormItem>
+
+                    <div className="flex flex-col gap-5 md:flex-row md:justify-between">
+                      <Button variant={"flat"} color={"primary"} onClick={() => setPage(2)}>
+                        Back
+                      </Button>
+                      <Button variant={"flat"} color={"primary"} onClick={handleSubmit}>
+                        Submit
+                      </Button>
                     </div>
-                  </SelectItem>
-                )}
-              </Select>
-              <Select
-                size="sm"
-                items={[{ "value": "Dark", "label": "Dark" }, { "value": "Light", "label": "Light" }]}
-                label="Color Theme"
-                placeholder="Select Color Theme"
-                className="max-w-xs"
-                value={colorTheme}
-                onChange={(e) => setColorTheme(e.target.value)}
-              >
-                {(theme) => <SelectItem key={theme.value}>{theme.label}</SelectItem>}
-              </Select>
-              <Textarea
-                label="Tell us something about you"
-                placeholder="I am a ..."
-                className="max-w-xs"
-              />
+
+                  </>
+                ) : null}
+              </Form>
             </div>
-            <div className='flex flex-col items-center gap-3'>
-              <Input
-                type="text"
-                variant="faded"
-                label="Last Name"
-                placeholder="Enter Your Last Name"
-                size="sm"
-                className="w-80 text-black m-2"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-              <Input
-                type="phone"
-                variant="faded"
-                label="Phone"
-                placeholder="Enter Phone Number"
-                size="sm"
-                className="w-80 text-black m-2"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-              />
-              <Select
-                size="sm"
-                items={[{ "value": "Male", "label": "Male" }, { "value": "Female", "label": "female" }]}
-                label="Gender"
-                placeholder="Select Gender"
-                className="max-w-xs"
-                value={selectedGender}
-                onChange={(e) => setSelectedGender(e.target.value)}
-              >
-                {(gender) => <SelectItem key={gender.value}>{gender.label}</SelectItem>}
-              </Select>
-              <Input
-                type="password"
-                variant="faded"
-                label="Password"
-                placeholder="Enter Your Password"
-                size="sm"
-                className="w-80 text-black m-2"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Input
-                type="password"
-                variant="faded"
-                label="Confirm Password"
-                placeholder="Confirm Your Password"
-                size="sm"
-                className="w-80 text-black m-2"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-              <Button color="primary" className='w-52 mt-5' variant="shadow" type="submit">
-                Sign Up
-              </Button>
+            <div className={"block md:hidden text-center"}>
+              <p style={{ fontSize: "10px" }} className={"mb-2 mt-3 dark:text-darkSecondary"}>
+                ©️ {new Date().getFullYear()} DPacks. All rights reserved.
+              </p>
             </div>
-            <Divider className='w-fit mt-5' />
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-
-export default Signup;
