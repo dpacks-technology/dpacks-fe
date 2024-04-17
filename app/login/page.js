18e1a53a582db7cbb1caf7e8d174482afcaf4953
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-04-17 09:42:29 +0530

    chore: login loading and redirection

--- app/login/page.js
+++ app/login/page.js
@@ -16,7 +16,6 @@ export default function LoginPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState({});
-    const [loading, setLoading] = useState(true); // loading state
 
     // backend validation error message
     const [messageApi, contextHolder] = message.useMessage(); // message api
@@ -27,16 +26,15 @@ export default function LoginPage() {
         });
     };
 
+    // check if the user is already authenticated
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
-            setLoading(false);
             router.push('/u');
-        } else {
-            setLoading(false);
         }
     }, [router]);
 
+    // login function
     const Login = async () => {
         setError({});
 
@@ -72,7 +70,7 @@ export default function LoginPage() {
                 }
             });
 
-        } catch (validationError) {
+        } catch (validationError) { // set error
             // set error
             let errorsObject = {}
             validationError.errors && validationError.errors.map(obj => errorsObject[Object.keys(obj)[0]] = Object.values(obj)[0]);
@@ -80,6 +78,7 @@ export default function LoginPage() {
         }
     }
 
+    // check the status of the server
     useEffect(() => {
         StatusService().then((response) => {
             console.log(response);
@@ -88,78 +87,80 @@ export default function LoginPage() {
         });
     }, []);
 
-    if (loading) {
+    // loading screen
+    if (localStorage.getItem('token')) {
         return <LoadingScreen/>
-    }
-
-    return (
-        <>
-            {contextHolder}
-            <div className={"w-full con-mid p-6 lg:p-0 overflow-hidden"} style={{minHeight: "100vh"}}>
-                <div className={"dark:bg-secondaryDark w-full sm:w-3/4 lg:w-1/2 p-6 lg:p-12 rounded-2xl"}>
-                    <div className={"md:grid md:grid-cols-3 md:gap-12"}>
-                        <div className={"col-span-1 p-2"}>
-                            <div className={"w-full text-left"}>
-                                <div className={"md:grid grid-cols-1 md:gap-24"}>
-                                    <div>
-                                        <img src={"/images/logos/dpacks-logo-white.png"}
-                                             className={"w-1/2 sm:w-1/2 md:w-3/4 lg:w-full xl:w-4/5"}
-                                             alt={"DPacks Logo"}/>
-                                        <h1 className={"dark:text-dark mt-3 text-md sm:text-lg mb-3 sm:mb-0"}>Sign
-                                            in</h1>
+    } else {
+        return (
+                <>
+                    {contextHolder}
+                    <div className={"w-full con-mid p-6 lg:p-0 overflow-hidden"} style={{minHeight: "100vh"}}>
+                        <div className={"dark:bg-secondaryDark w-full sm:w-3/4 lg:w-1/2 p-6 lg:p-12 rounded-2xl"}>
+                            <div className={"md:grid md:grid-cols-3 md:gap-12"}>
+                                <div className={"col-span-1 p-2"}>
+                                    <div className={"w-full text-left"}>
+                                        <div className={"md:grid grid-cols-1 md:gap-24"}>
+                                            <div>
+                                                <img src={"/images/logos/dpacks-logo-white.png"}
+                                                     className={"w-1/2 sm:w-1/2 md:w-3/4 lg:w-full xl:w-4/5"}
+                                                     alt={"DPacks Logo"}/>
+                                                <h1 className={"dark:text-dark mt-3 text-md sm:text-lg mb-3 sm:mb-0"}>Sign
+                                                    in</h1>
+                                            </div>
+                                            <div className={"hidden md:block"}>
+                                                <p style={{fontSize: "10px"}} className={"mb-2"}>
+                                                    © {new Date().getFullYear()} DPacks. All rights reserved.
+                                                </p>
+                                                <p style={{fontSize: "9px"}} className={"dark:text-darkSecondary"}>
+                                                    Static data management and global distribution technology.
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className={"hidden md:block"}>
-                                        <p style={{fontSize: "10px"}} className={"mb-2"}>
-                                            © {new Date().getFullYear()} DPacks. All rights reserved.
-                                        </p>
-                                        <p style={{fontSize: "9px"}} className={"dark:text-darkSecondary"}>
-                                            Static data management and global distribution technology.
-                                        </p>
-                                    </div>
+                                </div>
+                                <div className={"col-span-2 p-2"}>
+                                    <Form>
+                                        <FormItem>
+                                            <Input
+                                                name={"email"}
+                                                id={"email"}
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                label={"Email"}
+                                                className={"w-full"}
+                                                placeholder={"Email"}
+                                                status={error.email ? "error" : ""}
+                                                error={error.email}
+                                            />
+                                        </FormItem>
+                                        <FormItem>
+                                            <Input
+                                                name={"password"}
+                                                id={"password"}
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                label={"Password"}
+                                                className={"w-full"}
+                                                placeholder={"Password"}
+                                                type={"password"}
+                                                status={error.password ? "error" : ""}
+                                                error={error.password}
+                                            />
+                                        </FormItem>
+                                        <Button variant={"flat"} color={"primary"} className={"w-full mt-2"}
+                                                onClick={Login}>Sign
+                                            in</Button>
+                                    </Form>
+                                </div>
+                                <div className={"block md:hidden text-center"}>
+                                    <p style={{fontSize: "10px"}} className={"mb-2 mt-3 dark:text-darkSecondary"}>
+                                        © {new Date().getFullYear()} DPacks. All rights reserved.
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                        <div className={"col-span-2 p-2"}>
-                            <Form>
-                                <FormItem>
-                                    <Input
-                                        name={"email"}
-                                        id={"email"}
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        label={"Email"}
-                                        className={"w-full"}
-                                        placeholder={"Email"}
-                                        status={error.email ? "error" : ""}
-                                        error={error.email}
-                                    />
-                                </FormItem>
-                                <FormItem>
-                                    <Input
-                                        name={"password"}
-                                        id={"password"}
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        label={"Password"}
-                                        className={"w-full"}
-                                        placeholder={"Password"}
-                                        type={"password"}
-                                        status={error.password ? "error" : ""}
-                                        error={error.password}
-                                    />
-                                </FormItem>
-                                <Button variant={"flat"} color={"primary"} className={"w-full mt-2"} onClick={Login}>Sign
-                                    in</Button>
-                            </Form>
-                        </div>
-                        <div className={"block md:hidden text-center"}>
-                            <p style={{fontSize: "10px"}} className={"mb-2 mt-3 dark:text-darkSecondary"}>
-                                © {new Date().getFullYear()} DPacks. All rights reserved.
-                            </p>
-                        </div>
                     </div>
-                </div>
-            </div>
-        </>
-    );
+                </>
+        );
+    }
 }
