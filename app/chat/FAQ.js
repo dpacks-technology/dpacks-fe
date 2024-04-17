b85bececd08a9998368f5fd6ba058e9aea7f0f39
Nayananjalee <thulasikanayananjalee@gmail.com>
2024-04-17 12:46:31 +0530

    feat: visitor chat

---
+++ app/chat/FAQ.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FAQ = () => {
+    // You can add your FAQ content here as components or data.
+    return (
+        <div
+            style={{
+                padding: '10px',
+            }}
+        >
+            <h2 style={{color:'black'}}>FAQ Component</h2>
+            <p>fgfghvghvjgjvhvhhg</p>
+            {/* Add your FAQ content here */}
+        </div>
+    );
+};
+
+export default FAQ;
