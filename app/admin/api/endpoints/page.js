18670dd9476b54e9d56b31b7a70973aa87354d0a
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-04-23 10:52:06 +0530

    fix: some issues in admin api management

--- app/admin/api/endpoints/page.js
+++ app/admin/api/endpoints/page.js
@@ -42,6 +42,20 @@ export default function Webpages() {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
     const [searchColumn, setSearchColumn] = React.useState(sortColumn.column); // default search column
 
+    const components = {
+        status: true, // status component
+        columns: true, // columns component
+        refresh: true, // refresh component
+        bulk_actions: true, // bulk actions component
+        all: true, // all components
+        today: true, // today component
+        yesterday: true, // yesterday component
+        search: true, // search component
+        date_range: true, // date range component
+        export: true, // export component
+    }
+
+
     // ----------------------- COLUMNS -------------------------
     // columns // TODO: Change the following columns according the to yours
     const columns = [
@@ -378,6 +392,9 @@ export default function Webpages() {
                 dataCount={pagesCount}
                 rowsPerPage={rowsPerPage}
                 changeRowsPerPage={changeRowsPerPage}
+
+                // components
+                components={components}
 
             />
         </>
