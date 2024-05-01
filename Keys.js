3d818b0aa64e0eb864c22d6e07ff06855f88abd1
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-05-01 20:40:45 +0530

    Update Keys.js

--- Keys.js
+++ Keys.js
@@ -3,12 +3,12 @@ const Key = {
     ADMIN_SERVICE_API_URL: 'http://localhost:4001',
     MESSAGE_SERVICE_API_URL: 'http://localhost:4006',
     AUTH_SERVICE_API_URL: 'https://auth.dpacks.net',
-    DATA_PACKETS_SERVICE_API_URL: 'http://localhost:4000',
+    DATA_PACKETS_SERVICE_API_URL: 'https://web.dpacks.net',
 
-    // STORAGE_BUCKET_URL: 'https://web.dpacks.net/api/v1/data-packets/fetch',
-    STORAGE_BUCKET_URL: 'http://localhost:4000/api/v1/data-packets/fetch',
+    STORAGE_BUCKET_URL: 'https://web.dpacks.net/api/v1/data-packets/fetch',
+    // STORAGE_BUCKET_URL: 'http://localhost:4000/api/v1/data-packets/fetch',
 
-    CONNECTOR_URL: 'https://cdn.jsdelivr.net/gh/dpacks-technology/dpacks-connector-js/0.2.1-alpha.js'
+    CONNECTOR_URL: 'https://cdn.jsdelivr.net/gh/dpacks-technology/dpacks-connector-js/0.2.11-alpha.js'
 }
 
 module.exports = Key
