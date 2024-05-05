38778b79b2fa2a7eb479a73c00401c8b31841b59
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-05-06 00:18:16 +0530

    fix: templates

--- app/admin/marketplace/pending-templates/page.js
+++ app/admin/marketplace/pending-templates/page.js
@@ -101,21 +101,6 @@ export default function ReviewTemplates() {
         "menu",
     ];
 
-    // ----------------------- COMPONENTS -------------------------
-    // components // TODO: Change the following components
-    const components = {
-        status: true, // status component
-        columns: true, // columns component
-        refresh: true, // refresh component
-        bulk_actions: true, // bulk actions component
-        all: true, // all components
-        today: true, // today component
-        yesterday: true, // yesterday component
-        search: true, // search component
-        date_range: true, // date range component
-        export: true, // export component
-    }
-
     // ----------------------- BUTTONS -------------------------
     // 1. action buttons (buttons)
     /***
