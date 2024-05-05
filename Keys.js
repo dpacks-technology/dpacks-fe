81763314b390383e877c81f929db40c3528207dc
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-05-05 23:00:58 +0530

    Update Keys.js

--- Keys.js
+++ Keys.js
@@ -18,7 +18,7 @@ const Key = {
     AI_STORAGE_BUCKET_URL: 'https://web.dpacks.net/api/v1/data-packets/fetch-ai',
 
     // connector url
-    CONNECTOR_URL: 'https://cdn.jsdelivr.net/gh/dpacks-technology/dpacks-connector-js/0.2.16-alpha.js'
+    CONNECTOR_URL: 'https://cdn.jsdelivr.net/gh/dpacks-technology/dpacks-connector-js/0.2.17-alpha.js'
 }
 
 module.exports = Key
