e7de62f54cd72135911536a391f76f3123594b1d
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-05-05 23:17:28 +0530

    fix: analytics navigation

--- app/data/UserDashboardNavigation.jsx
+++ app/data/UserDashboardNavigation.jsx
@@ -54,7 +54,7 @@ export const UserDashboardNavigation =
                 <path strokeLinecap="round" strokeLinejoin="round"
                       d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"/>
             </svg>,
-            url: "../Analytics",
+            url: "../Analytics/overview",
             children: [
 
                 // Analytics -> overview
@@ -65,7 +65,7 @@ export const UserDashboardNavigation =
                         <path strokeLinecap="round" strokeLinejoin="round"
                               d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"/>
                     </svg>,
-                    url: "../Analytics",
+                    url: "../Analytics/overview",
                 },
                 
                 {
